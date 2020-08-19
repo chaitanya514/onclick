7f9389d6bddaec89bdd0e29583d821d396bfaf5f
chaipa <chaitanya.patel@flemingcollege.ca>
2020-08-19 14:14:22 -0400

    onclcik works

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
 import './App.css';
 
 function App() {
+  const [name, setName] = useState({
+    firstName: "",
+    lastName: ""
+  })
+
+  const [submit, setSubmit] = useState([])
+
+
+  const handleChange = (event) => {
+    setName({
+      ...name,
+      [event.target.name]: event.target.value
+    })
+  }
+
+  const handleSubmit = (event) => {
+let firstName =name.firstName;
+let lastName = name.lastName;
+    event.preventDefault();
+    setSubmit([
+      ...submit,
+      firstName,
+      lastName
+    ])
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <label>First Name  </label> <input type="text" name="firstName" onChange={handleChange} /><br />
+      <label>Last Name  </label> <input type="text" name="lastName" onChange={handleChange} onKeyPress={handleSubmit} />
+        <button type="submit" >Submit</button>
+
+  {submit}
+  
+      </form>
     </div>
   );
 }
